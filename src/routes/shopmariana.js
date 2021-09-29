ae975bd67f61c23f6821e8644f0704068562aa38
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-09-29 08:29:40 -0500

    se crean vistas para signup y signin

--- src/routes/shopmariana.js
+++ src/routes/shopmariana.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+
 router.get('/', (req, res, next) => {
-    
+    res.render('shopView');
 });
 
 router.get('/signup', (req, res, next) => {
-    
+    res.render('signup')
 });
 
 router.post('/signup', (req, res, next) => {
